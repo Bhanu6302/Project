60de3dc5b1dc4a784bcb022fae3a97f292bd1f1d
Amasa Bhanu Prakash <bhanuprakashreddy6302@gmail.com>
2025-04-02 10:08:53 +0530

    package.json changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./ecommerce_project/Header";
 import ProductList from "./ecommerce_project/Product";
 import Cart from "./ecommerce_project/Cart";
-// import NotFound from "./ecommerce_project/Notfound";
+import NotFound from "./ecommerce_project/Notfound";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./ecommerce_project/Store";
@@ -15,7 +15,7 @@ const App = () => (
       <Routes>
         <Route path="/" element={<ProductList />} />
         <Route path="/cart" element={<Cart />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   </Provider>
